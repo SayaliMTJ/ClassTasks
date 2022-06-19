56233a3e0e603ea394ebbc18432ba15ef2ba2c60
Sayali Mammadova <sayali.mammadli@gmail.com>
2022-06-18 21:20:46 -0700

    initial commit

--- src/Task_Facebook.java
+++ src/Task_Facebook.java
@@ -1,11 +1,9 @@
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 
-public class Facebook {
+public class Task_Facebook {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/sayalimammadova/Desktop/BrowserDrivers/chromedriver");
         WebDriver driver = new ChromeDriver();
